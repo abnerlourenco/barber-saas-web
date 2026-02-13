3c46420c4283210b4d6a25a04448c823c7fb6d35
Abner da Rocha Lourenço <abner2010_hm@hotmail.com>
2026-02-13 11:36:36 -0300

    feat: add header components and banner

---
+++ app/_components/header.tsx
@@ -0,0 +1,17 @@
+import { MenuIcon } from "lucide-react"
+import Image from "next/image"
+import { Button } from "./ui/button"
+import { Card, CardContent } from "./ui/card"
+
+export default function Header() {
+  return (
+    <Card>
+      <CardContent className={"flex flex-row items-center justify-between"}>
+        <Image alt="FSW Barber" src="/logo.png" height={18} width={120} />
+        <Button size="icon" variant="outline">
+          <MenuIcon />
+        </Button>
+      </CardContent>
+    </Card>
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,33 @@
+import { SearchIcon } from "lucide-react"
+import Image from "next/image"
+import Header from "./_components/header"
+import { Button } from "./_components/ui/button"
+import { Input } from "./_components/ui/input"
+
 export default function Home() {
   return (
     <div>
-      <h1>Hello World!!</h1>
+      <Header />
+      <div className="p-5">
+        <h2 className="text-xl font-bold">Olá, Abner!</h2>
+        <p>Quinta-Feira, 13 de Fevereiro.</p>
+
+        <div className="mt-6 flex items-center gap-2">
+          <Input placeholder="Faça sua busca..." />
+          <Button>
+            <SearchIcon />
+          </Button>
+        </div>
+
+        <div className="relative mt-6 h-45 w-full">
+          <Image
+            alt="Agende nas melhores com FSW Barber"
+            src="/banner-01.png"
+            fill
+            className="rounded-xl object-cover"
+          />
+        </div>
+      </div>
     </div>
   )
 }
