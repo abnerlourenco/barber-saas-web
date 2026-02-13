c9288980e0e2344a5a15175567eb711201037ef0
Abner da Rocha Lourenço <abner2010_hm@hotmail.com>
2026-02-13 17:17:01 -0300

    feat: add booking card

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
 import { SearchIcon } from "lucide-react"
 import Image from "next/image"
 import Header from "./_components/header"
+import { Avatar, AvatarImage } from "./_components/ui/avatar"
+import { Badge } from "./_components/ui/badge"
 import { Button } from "./_components/ui/button"
+import { Card, CardContent } from "./_components/ui/card"
 import { Input } from "./_components/ui/input"
 
 export default function Home() {
@@ -27,6 +30,33 @@ export default function Home() {
             className="rounded-xl object-cover"
           />
         </div>
+
+        <h2 className="mt-8 text-xs font-bold uppercase">Agendamentos</h2>
+
+        <Card className="mt-4 p-0">
+          <CardContent className="flex justify-between p-0">
+            <div className="flex flex-col gap-2 py-5 pl-5">
+              <Badge>Confirmado</Badge>
+              <h1 className="font-semibold">Corte de Cabelo</h1>
+
+              <div className="flex items-center gap-2">
+                <Avatar>
+                  <AvatarImage
+                    alt="Avatar image"
+                    src="https://github.com/evilrabbit.png"
+                  />
+                </Avatar>
+                <p className="text-sm">Tião Barbeiro</p>
+              </div>
+            </div>
+
+            <div className="flex flex-col items-center justify-center border-l-2 border-solid px-10">
+              <p className="text-md">Fevereiro</p>
+              <strong className="text-3xl">13</strong>
+              <p className="text-sm">09:45</p>
+            </div>
+          </CardContent>
+        </Card>
       </div>
     </div>
   )
