cd39975d3154e8f501f2c57b8e8cd84a8175cf3c
Abner da Rocha Lourenço <abner2010_hm@hotmail.com>
2026-05-29 17:58:29 -0300

    feat: add dark mode support for icons and update fast search button
    - Added dark mode SVG icons.
    - Updated fast search options to include dark mode image URLs.
    - Modified FastSearchButton component to render appropriate icons based on theme.
    - Introduced a new Sheet component for the header menu with user avatar and navigation links.

--- src/_components/fast-search-button.tsx
+++ src/_components/fast-search-button.tsx
@@ -4,32 +4,39 @@ import { Button } from "./ui/button"
 interface fastSearchOptions {
   imageUrl: string
   title: string
+  darkImageUrl: string
 }
 
-const fastSearchOptions: fastSearchOptions[] = [
+export const fastSearchOptions: fastSearchOptions[] = [
   {
     imageUrl: "/cabelo.svg",
     title: "Cabelo",
+    darkImageUrl: "/cabelo_dark.svg",
   },
   {
     imageUrl: "/barba.svg",
     title: "Barba",
+    darkImageUrl: "/barba_dark.svg",
   },
   {
     imageUrl: "/hidratacao.svg",
     title: "Hidratação",
+    darkImageUrl: "/hidratacao_dark.svg",
   },
   {
     imageUrl: "/massagem.svg",
     title: "Massagem",
+    darkImageUrl: "/massagem_dark.svg",
   },
   {
     imageUrl: "/sobrancelha.svg",
     title: "Sobrancelha",
+    darkImageUrl: "/sobrancelha_dark.svg",
   },
   {
     imageUrl: "/acabamento.svg",
     title: "Acabamento",
+    darkImageUrl: "/acabamento_dark.svg",
   },
 ]
 
@@ -42,12 +49,22 @@ export default function FastSearchButton() {
           variant="secondary"
           key={option.title}
         >
+          {/* Image ligth theme */}
           <Image
             src={option.imageUrl}
             width={16}
             height={16}
             alt={option.title}
-            className="fill-primary"
+            className="block dark:hidden"
+          />
+
+          {/* Image dark theme*/}
+          <Image
+            src={option.darkImageUrl || option.imageUrl}
+            width={16}
+            height={16}
+            alt={option.title}
+            className="hidden dark:block"
           />
           {option.title}
         </Button>
--- src/_components/header.tsx
+++ src/_components/header.tsx
@@ -1,7 +1,18 @@
-import { MenuIcon } from "lucide-react"
+import { CalendarIcon, HomeIcon, LogOutIcon, MenuIcon } from "lucide-react"
 import Image from "next/image"
+import Link from "next/link"
+import { fastSearchOptions } from "./fast-search-button"
+import { Avatar, AvatarImage } from "./ui/avatar"
 import { Button } from "./ui/button"
 import { Card, CardContent } from "./ui/card"
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "./ui/sheet"
 
 export default function Header() {
   return (
@@ -9,9 +20,95 @@ export default function Header() {
       <Card className="custom-card rounded-t-none border-none">
         <CardContent className={"flex flex-row items-center justify-between"}>
           <Image alt="FSW Barber" src="/logo.png" height={18} width={120} />
-          <Button size="icon" variant="outline">
-            <MenuIcon />
-          </Button>
+
+          <Sheet>
+            <SheetTrigger asChild>
+              <Button size="icon" variant="outline">
+                <MenuIcon />
+              </Button>
+            </SheetTrigger>
+            <SheetContent className="min-w-70 overflow-y-auto md:max-w-87.5">
+              <SheetHeader>
+                <SheetTitle className="text-left">Menu</SheetTitle>
+              </SheetHeader>
+
+              {/* Avatar */}
+              <div className="flex items-center gap-3 border-b border-solid px-5 pb-5">
+                <Avatar className="h-16 w-16">
+                  <AvatarImage src="https://github.com/abnerlourenco.png" />
+                </Avatar>
+                <div>
+                  <p className="font-bold">Abner Lourenco</p>
+                  <p className="text-sm">abner@barbershop.com</p>
+                </div>
+              </div>
+
+              {/* Bottons */}
+              <div className="flex flex-col gap-2 border-b border-solid px-5 pb-5">
+                <SheetClose asChild>
+                  <Button
+                    className="h-11 justify-start gap-2"
+                    variant={"ghost"}
+                    asChild
+                  >
+                    <Link href={"/"}>
+                      <HomeIcon size={18} />
+                      Inicio
+                    </Link>
+                  </Button>
+                </SheetClose>
+                <Button
+                  className="h-11 justify-start gap-2"
+                  variant={"ghost"}
+                  asChild
+                >
+                  <Link href={"/agendamentos"}>
+                    <CalendarIcon size={18} />
+                    Agendamentos
+                  </Link>
+                </Button>
+              </div>
+
+              {/* QuickAccess */}
+              <div className="flex flex-col gap-2 border-b border-solid px-5 pb-5">
+                {fastSearchOptions.map((options) => (
+                  <Button
+                    variant={"ghost"}
+                    className="h-11 justify-start gap-2"
+                    key={options.title}
+                  >
+                    {/* Image ligth theme */}
+                    <Image
+                      alt={options.title}
+                      src={options.imageUrl}
+                      width={18}
+                      height={18}
+                      className="block object-contain dark:hidden"
+                    />
+
+                    {/* Image dark theme*/}
+                    <Image
+                      alt={options.title}
+                      src={options.darkImageUrl || options.imageUrl}
+                      width={18}
+                      height={18}
+                      className="hidden object-contain dark:block"
+                    />
+
+                    {options.title}
+                  </Button>
+                ))}
+              </div>
+
+              {/* Exit button */}
+              <div className="flex flex-col gap-2 px-5 pb-5">
+                <Button className="h-11 justify-start gap-2" variant={"ghost"}>
+                  <LogOutIcon size={18} />
+                  Sair da conta
+                </Button>
+              </div>
+            </SheetContent>
+          </Sheet>
         </CardContent>
       </Card>
     </header>
