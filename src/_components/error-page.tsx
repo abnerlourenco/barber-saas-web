6a37e6ded952fd5d9d552218019cf3903ca495ff
Abner da Rocha Lourenço <abner2010_hm@hotmail.com>
2026-06-08 16:42:06 -0300

    feat: layout ajusts to manager footer and error page

--- src/_components/error-page.tsx
+++ src/_components/error-page.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link"
+import Header from "./header"
 import { Button } from "./ui/button"
 
 interface ErrorPageProps {
@@ -8,22 +9,27 @@ interface ErrorPageProps {
 
 export default function ErrorPage(props: ErrorPageProps) {
   return (
-    <div className="flex flex-col items-center justify-center text-sm max-md:px-4">
-      <h1 className="text-primary text-8xl font-bold md:text-9xl">
-        {props.errorCode}
-      </h1>
-      <div className="bg-secondary my-5 h-1 w-16 rounded md:my-7"></div>
-      <p className="text-2xl font-bold text-gray-800 md:text-3xl">
-        {props.title}
-      </p>
-      <p className="text-foreground mt-4 max-w-md py-4 text-center text-sm md:text-base">
-        A Página que você acessou pode ter sido removida, alterada, ou
-        temporariamente indisponível.
-      </p>
-      <div className="bg-secondary my-5 h-1 w-16 rounded md:my-7"></div>
-      <Button size="lg">
-        <Link href={"/"}>Retornar</Link>
-      </Button>
+    <div>
+      <Header />
+      <div className="mt-6">
+        <div className="flex flex-col items-center justify-center text-sm max-md:px-4">
+          <h1 className="text-primary text-8xl font-bold md:text-9xl">
+            {props.errorCode}
+          </h1>
+          <div className="bg-secondary my-5 h-1 w-16 rounded md:my-7"></div>
+          <p className="text-2xl font-bold text-gray-800 md:text-3xl">
+            {props.title}
+          </p>
+          <p className="text-foreground mt-4 max-w-md py-4 text-center text-sm md:text-base">
+            A Página que você acessou pode ter sido removida, alterada, ou
+            temporariamente indisponível.
+          </p>
+          <div className="bg-secondary my-5 h-1 w-16 rounded md:my-7"></div>
+          <Button size="lg">
+            <Link href={"/"}>Retornar</Link>
+          </Button>
+        </div>
+      </div>
     </div>
   )
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,9 +30,11 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <AuthProvider>
-          {children}
+          <div className="flex h-full flex-col">
+            <div className="flex-1">{children}</div>
 
-          <Footer />
+            <Footer />
+          </div>
         </AuthProvider>
       </body>
     </html>
